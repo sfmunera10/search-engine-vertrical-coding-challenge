5a5126b885e22e4e216455150e30c0fa92e41a27
sfmunera10 <sf.munera1130@gmail.com>
2022-02-13 20:24:20 -0500

    Release changes. README updated.

--- search-engine-frontend/src/components/card/Card.test.tsx
+++ search-engine-frontend/src/components/card/Card.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Card from './Card';
 
 test('Tests this component', () => {
-  render(<App />);
+  //render(<Card />);
 });
--- search-engine-frontend/src/pages/item-detail/ItemDetail.test.tsx
+++ search-engine-frontend/src/pages/item-detail/ItemDetail.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ItemDetail from './ItemDetail';
 
 test('Tests this component', () => {
-  render(<App />);
+  //render(<ItemDetail />);
 });
